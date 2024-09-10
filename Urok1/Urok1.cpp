c9f798ff9eac99b15af0aed66e6a87dd75f09398
234842 <234842@KB37-309-C05>
2024-09-10 13:22:49 +0300

    Добавьте файлы проекта.

---
+++ Urok1/Urok1.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+int integ = 25;
+void main()
+{
+	char w[] = "west";
+	int in = 20;
+	string s1 = "hello";
+	char ch1 = 's';
+	double d1 = 25;
+	float f1 = 25.6;
+	int a1;
+	char cha = 'A';
+	a1 = 25;
+	char a11 = 'H';
+	string str = "Hello";
+	char b11 = 'B';
+	int c3 = 18;
+	s1 = 'hi';
+	char s[] = "lets go";
+	{ int inerg = 22; }
+	cout << integ << endl; // << inerg << endl;
+}
